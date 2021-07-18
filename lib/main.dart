aa2cb471e0544d744abef910389bba2899098e60
Nilton Firmino <87314150+NiltonFirmino@users.noreply.github.com>
2021-07-18 10:27:51 -0300

    Creating Lists
    Criando os Funcionamentos das Listas

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -6,18 +10,33 @@ void main() {
     home: Home(),
   ));
 }
+
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  // lista para armazenar as tarefas
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
+}
+
+//Função que retorna o arquivo/lista que será utilizado pra salvar
+Future<File> _getFile() async {
+  final directory = await getApplicationDocumentsDirectory();
+  return File("${directoy.path}/data.json");
+}
+
+// Função para salvar dentro do arquivo/lista
+Future<File> _saveData() async {
+  String data = json.encode(_toDoList);
+  final file = await _getFile();
+  return file.writeAsString(data);
 }

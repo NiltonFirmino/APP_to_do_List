5161e551c197687d61787f330dcd99d5ca386f78
Nilton Firmino <87314150+NiltonFirmino@users.noreply.github.com>
2021-07-20 10:12:02 -0300

    correction of JSON list creation
    Realizado leves correções e conclusão do JSON

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:core';
 import 'dart:io';
 import 'dart:async';
 
@@ -26,17 +27,26 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Container();
   }
-}
 
-//Função que retorna o arquivo/lista que será utilizado pra salvar
-Future<File> _getFile() async {
-  final directory = await getApplicationDocumentsDirectory();
-  return File("${directoy.path}/data.json");
-}
+  //Função que retorna o arquivo/lista que será utilizado pra salvar
+  Future<File> _getFile() async {
+    final directoy = await getApplicationDocumentsDirectory();
+    return File("${directoy.path}/data.json");
+  }
 
-// Função para salvar dentro do arquivo/lista
-Future<File> _saveData() async {
-  String data = json.encode(_toDoList);
-  final file = await _getFile();
-  return file.writeAsString(data);
+  // Função para salvar dentro do arquivo/lista
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+  // Função para obter os dados do arquivo/lista
+  Future<String?> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+        return null;
+    }
+  }
 }

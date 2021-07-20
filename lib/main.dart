46255995a3e90a1497c4ab0561e8faedb1b6e441
Nilton Firmino <87314150+NiltonFirmino@users.noreply.github.com>
2021-07-20 15:07:39 -0300

    code reorganization

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:core';
 import 'dart:io';
 import 'dart:async';
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -31,36 +30,41 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.deepPurpleAccent,
         
       ),
+      
       body: Column(
         children: <Widget>[
-        Container(
-          //EdgeInsets.fromLTRB(esquerda, cima, direita, baixo)
-          padding: EdgeInsets.fromLTRB(17.0,1.0, 7.0, 1.0), 
-          child: Row(
-            children: <Widget> [
-              Expanded(child:  TextField(
+          Container(
+            //EdgeInsets.fromLTRB(esquerda, cima, direita, baixo)
+            padding: EdgeInsets.fromLTRB(17.0,1.0, 7.0, 1.0), 
+            child: Row(
+              children: <Widget> [
+                Expanded(child:  TextField(
                   decoration: InputDecoration(
                   labelText: "Nova Tarefa",
                   labelStyle: 
                     TextStyle(color: Colors.deepPurple),
 
-                 ),
+                    ),
+                  ),
                 ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                primary: Colors.deepPurpleAccent, // background
-                onPrimary: Colors.white, // cor do texto
-                ),
-                onPressed: () { },
-                child: Text('ADD'),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                  primary: Colors.deepPurpleAccent, // background
+                  onPrimary: Colors.white, // cor do texto
+                  ),
+                  onPressed: () { },
+                  child: Text('ADD'),
                 )
-            ],),
+              ],
+            ),
         )
-      ],),
+        ],
+      ),
     );
   }
-Future<File> _getFile() async {
+
+  //Função que retorna o arquivo/lista que será utilizado pra salvar
+  Future<File> _getFile() async {
     final directoy = await getApplicationDocumentsDirectory();
     return File("${directoy.path}/data.json");
   }
